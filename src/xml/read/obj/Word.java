0319c266778a0a2f052c62def0c5fdb1fbdf29a5
Jenda <nocrush926@gmail.com>
2017-10-17 15:33:15 +0700

    add constructor and some methods

--- src/xml/read/obj/Word.java
+++ src/xml/read/obj/Word.java
@@ -1,5 +1,19 @@
 package xml.read.obj;
 
 public class Word {
+	private String HeadWord;
 
+	public Word(String headWord) {
+		super();
+		HeadWord = headWord;
+	}
+
+	public String getHeadWord() {
+		return HeadWord;
+	}
+
+	public void setHeadWord(String headWord) {
+		HeadWord = headWord;
+	}
+	
 }
